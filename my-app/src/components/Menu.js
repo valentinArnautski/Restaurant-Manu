6afc54d66aaa1a269d038c86328675ea0a0ae31f
ValentinAr7 <valio990210@gmail.com>
2023-07-19 18:18:03 +0300

    [Menu] Buttons style and functionality

--- my-app/src/components/Menu.js
+++ my-app/src/components/Menu.js
@@ -4,53 +4,54 @@ import styles from './menu.module.css';
 import { theme } from './colorTheme';
 
 export const Menu = () => {
-  const [activeMenuItem, setActiveMenuItem] = useState('');
+    const [activeMenuItem, setActiveMenuItem] = useState('');
 
-  const handleMenuItemClick = (menuItem) => {
-    setActiveMenuItem(menuItem);
-  };
+    const handleMenuItemClick = (menuItem) => {
+        setActiveMenuItem(menuItem);
+    };
 
-  return (
-    <ThemeProvider theme={theme}>
-    <Box className={styles.container}>
-      <Box className={styles.header}>
-        <h2>Blankfactor</h2>
-      </Box>
+    return (
+        <ThemeProvider theme={theme}>
+            <Box
+                className={styles.container}>
+                <Box className={styles.header}>
+                    <p className={styles.title}>Blankfactor</p>
+                </Box>
 
-      <div className={styles.menuNavContainer}>
-        <Box className={styles.menuNavigation}>
-          <Button
-            className={styles.btns}
-            variant={activeMenuItem === 'Ястия' ? 'contained' : 'text'}
-            onClick={() => handleMenuItemClick('Ястия')}
-          >
-            Ястия
-          </Button>
-          <Button
-            className={styles.btns}
-            variant={activeMenuItem === 'Напитки' ? 'contained' : 'text'}
-            onClick={() => handleMenuItemClick('Напитки')}
-          >
-            Напитки
-          </Button>
-          <Button
-            className={styles.btns}
-            variant={activeMenuItem === 'Вино' ? 'contained' : 'text'}
-            onClick={() => handleMenuItemClick('Вино')}
-          >
-            Вино
-          </Button>
-        </Box>
-      </div>
+                <div className={styles.menuNavContainer}>
+                    <Box className={styles.menuNavigation}>
+                        <Button
+                            className={styles.btns}
+                            variant={activeMenuItem === 'Ястия' ? 'contained' : 'text'}
+                            onClick={() => handleMenuItemClick('Ястия')}
+                        >
+                            Ястия
+                        </Button>
+                        <Button
+                            className={styles.btns}
+                            variant={activeMenuItem === 'Напитки' ? 'contained' : 'text'}
+                            onClick={() => handleMenuItemClick('Напитки')}
+                        >
+                            Напитки
+                        </Button>
+                        <Button
+                            className={styles.btns}
+                            variant={activeMenuItem === 'Вино' ? 'contained' : 'text'}
+                            onClick={() => handleMenuItemClick('Вино')}
+                        >
+                            Вино
+                        </Button>
+                    </Box>
+                </div>
 
-      <Box
-        className={styles.backgroundImage}
-        style={{
-          backgroundImage:
-            'url(https://images.unsplash.com/photo-1537047902294-62a40c20a6ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80)',
-        }}
-      ></Box>
-    </Box>
-    </ThemeProvider>
-  );
+                <Box
+                    className={styles.backgroundImage}
+                    style={{
+                        backgroundImage:
+                            'url(https://images.unsplash.com/photo-1537047902294-62a40c20a6ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80)',
+                    }}
+                ></Box>
+            </Box>
+        </ThemeProvider>
+    );
 };
--- my-app/src/components/colorTheme.js
+++ my-app/src/components/colorTheme.js
@@ -1,9 +1,18 @@
 import { createTheme } from "@mui/material";
+import { red } from "@mui/material/colors";
 
 export const theme = createTheme({
     palette: {
       primary: {
         main: '#ccc'
+      },
+      secondary: {
+        main: "#ff9800", // orange color
+        dark: "#ffd54f", // yellow color
+        contrastText: "#000",
+      },
+      error: {
+        main: red[500], // red color for delete button and errors
       },
     },
   });
