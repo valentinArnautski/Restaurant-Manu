e2154418cbff22d60bb36d3f7dadcfa97099c65a
ValentinAr7 <valio990210@gmail.com>
2023-07-20 17:59:16 +0300

    [Menu] Images

--- my-app/src/components/mockData.js
+++ my-app/src/components/mockData.js
@@ -21,27 +21,27 @@ const foodSections = [
       label: 'Пици',
       image: 'https://pngimg.com/uploads/pizza/pizza_PNG44095.png',
       dishes: [
-          { label: 'Pepperoni Pizza', weight: '400гр', price: '14.90лв', image: 'url_to_image_7' },
-          { label: 'Margherita Pizza', weight: '350гр', price: '13.50лв', image: 'url_to_image_8' },
-          { label: 'BBQ Chicken Pizza', weight: '400гр', price: '15.90лв', image: 'url_to_image_9' },
+          { label: 'Pepperoni Pizza', weight: '400гр', price: '14.90лв', image: 'https://thelionsroarmhsn.com/wp-content/uploads/2015/04/Pepperoni_1-300x200.jpg' },
+          { label: 'Margherita Pizza', weight: '350гр', price: '13.50лв', image: 'https://www.amomikitchenaid.com/wp-content/uploads/2017/02/BLOG-RECETA-PIZZA-MARGARITA-MAR17-1024x768.jpg' },
+          { label: 'BBQ Chicken Pizza', weight: '400гр', price: '15.90лв', image: 'http://1.bp.blogspot.com/-bmOXpNwdhYE/TfAhJxVXpHI/AAAAAAAAEBg/Xx5mda5NAfM/s400/DSC_6323.JPG' },
       ]
   },
   {
       label: 'Салати',
       image: 'https://www.cookingclassy.com/wp-content/uploads/2019/11/best-salad-7-768x1152.jpg',
       dishes: [
-          { label: 'Greek Salad', weight: '200гр', price: '8.90лв', image: 'url_to_image_10' },
-          { label: 'Caesar Salad', weight: '200гр', price: '9.50лв', image: 'url_to_image_11' },
-          { label: 'Caprese Salad', weight: '180гр', price: '8.50лв', image: 'url_to_image_12' },
+          { label: 'Greek Salad', weight: '200гр', price: '8.90лв', image: 'https://www.simplyrecipes.com/thmb/Ayk4nOl1Tn60OpDbn7iHvZqEiuA=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2011__08__dads-greek-salad-horiz-a-1600-bc71b85bc907414a9bb3179a923b58fc.jpg' },
+          { label: 'Caesar Salad', weight: '200гр', price: '9.50лв', image: 'https://asset.dr.dk/imagescaler01/http://mad-recipe-pictures-dr-dk.s3.amazonaws.com/prod/recipe/caesar-salad-1588416116.jpg&w=1368&h=770&scaleAfter=crop&quality=62&ratio=16-9' },
+          { label: 'Caprese Salad', weight: '180гр', price: '8.50лв', image: 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2019/07/Caprese-Salad-3-3.jpg' },
       ]
   },
   {
       label: 'Разядки',
       image: 'https://dolceamaro.bg/wp-content/uploads/2021/06/uzo-razqdki.jpg',
       dishes: [
-          { label: 'Cheese Plate', weight: '250гр', price: '15.90лв', image: 'url_to_image_13' },
-          { label: 'Mixed Olives', weight: '200гр', price: '8.50лв', image: 'url_to_image_14' },
-          { label: 'Charcuterie Platter', weight: '300гр', price: '19.90лв', image: 'url_to_image_15' },
+          { label: 'Cheese Plate', weight: '250гр', price: '15.90лв', image: 'https://i.pinimg.com/564x/46/9c/1a/469c1a60873434abb234752ad037339c.jpg' },
+          { label: 'Mixed Olives', weight: '200гр', price: '8.50лв', image: 'https://jeffsgardenfoods.com/wp-content/uploads/2019/07/Marinated-Mixed-Olives-with-Manchego-edited.jpg' },
+          { label: 'Charcuterie Platter', weight: '300гр', price: '19.90лв', image: 'https://fromagination.com/wp-content/uploads/2017/04/Artisan-Meat-Tray.a.14x10.72res.jpg' },
       ]
   },
 ];
